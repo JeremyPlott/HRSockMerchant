bb954a1dafd828ba1d456f7c8c13563ddfc0e8e1
Jeremy <JeremyPlott@gmail.com>
2019-09-12 12:22:05 -0400

    Add project files.

---
+++ HRCode/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the sockMerchant function below.
+    static int sockMerchant(int n, int[] ar) {
+
+        var dict = new Dictionary<int, int>();
+        foreach (int i in ar) {
+            if (dict.ContainsKey(i))
+                dict[i]++;
+            else
+                dict.Add(i, 1);
+        }
+
+        int sum = 0;
+        foreach (var entry in dict) {
+            int pairs = entry.Value / 2;
+            sum += pairs;
+        }
+        return sum;
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp))
+        ;
+        int result = sockMerchant(n, ar);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
---
+++ HRSockMerchant/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HRSockMerchant {
+    class Program {
+        static void Main(string[] args) {
+
+            var ar = new int[] { 1, 1, 1, 2, 2, 3, 4, 5, 5, 5, 5, 6, 6, 6 }; // 5 pairs - 1, 2, 5, 5, 6
+
+            int n = ar.Length;
+
+            var dict = new Dictionary<int, int>();
+            foreach (int i in ar) {
+                if (dict.ContainsKey(i))
+                    dict[i]++;
+                else
+                    dict.Add(i, 1);
+            }
+
+            int sum = 0;
+            foreach(var entry in dict) {
+                int pairs = entry.Value / 2;
+                sum += pairs;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
